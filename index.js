4ea3c3bcc0fa03d9a19ef05134e3cecb394e33ba
Gabriela Burlison <gabrielasiqueira014@gmail.com>
2025-12-24 17:25:15 -0900

    Add event listener

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 for (let index = 0; index < 16 * 16; index++) {
     const div = document.createElement("div");
     div.classList.add("grid-square");
+    div.addEventListener("mouseover", () => {
+        div.classList.add("trace");
+    });
     document.getElementById("container").appendChild(div);
-} 
+}
+
+
