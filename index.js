886bc2ec3827f03b213f1762796dd9276c991f90
Gabriela Burlison <gabrielasiqueira014@gmail.com>
2026-01-07 17:34:27 -0900

    Create initialCanva() and a the link to customize a brand new canva

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-//Fills the 16x16 container with squares
+//Fill a 16x16 container with squares
+function initialCanva(){
 for (let index = 0; index < 16 * 16; index++) {
     const div = document.createElement("div");
     div.classList.add("grid-square");
@@ -7,5 +8,28 @@ for (let index = 0; index < 16 * 16; index++) {
     });
     document.getElementById("container").appendChild(div);
 }
+};
 
+//Generate a brand new canvas customized by user's input
+document.getElementById("processButton").addEventListener("click", () => {
+    document.getElementById("container").innerHTML = "";
+    let size = document.getElementById("numberInput").value;
+    let n = parseInt(size);
+    if (n > 0 && n <= 100) {
+        for (let index = 0; index < n * n; index++) {
+            const div = document.createElement("div");
+            div.classList.add("grid-square");
+            div.style.width = `calc(100%/${n})`;
+            div.style.height = `calc(100%/${n})`;
+            document.getElementById("container").appendChild(div);
+            div.addEventListener("mouseover", () => {
+                div.classList.add("trace");
+            });
+        }
+    } else {
+        alert("Please, select a number between 0-100");
+        initialCanva();
+    }
+});
 
+initialCanva();
